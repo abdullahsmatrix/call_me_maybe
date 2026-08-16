15367ca7c4544e544d60c671794bca5a0370805e
abdullahsmatrix <abdullahalmamun9918@gmail.com>
2026-08-16 16:46:21 +0200

    arguement parser implemented

--- src/__main__.py
+++ src/__main__.py
@@ -3,26 +3,41 @@ import json
 import sys
 
 
-def parse_arguements():
+def parse_arguements() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="call_me_maybe",
-        description="LLM function calling tool that that translates"
+        description="LLM function calling tool that that translates "
                      "natural language prompts into structured function calls",
-        usage="uv run python -m src [--functions_definition "
-        "<function_definition_file>] [--input <input_file>] "
-        "[--output <output_file>]",
-        
         )
+
+    parser.add_argument(
+        "--functions_definition",
+        help = "Declare path to JSON file containing function definitions",
+        default= "data/input/functions_definition.json",
+        )
+    parser.add_argument(
+        "--input",
+        help="Declare path to input file containing the prompts",
+        default="data/input/function_calling_tests.json",
+        )
+    parser.add_argument(
+        "--output",
+        help="Declare path to JSON output file",
+        default="data/output/function_calls.json"
+        )
+
+    args = parser.parse_args()
+    return args
 
 def main() -> None:
 
-    #path = parse_arguements()
+    paths = parse_arguements()
 
-    path: str = "data/input/functions_definition.json"
     data: dict ={}
     try:
-        with open(path, "r") as file:
+        with open(paths.functions_definition, "r") as file:
             data = json.load(file)
+            print(data)
     except (FileNotFoundError, json.JSONDecodeError) as err:
         print(err)
         sys.exit(1)
